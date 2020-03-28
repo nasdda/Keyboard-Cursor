ecb17c26f6062a768028af8d89cdc3c78abe9188
Xin <xin2001yan@gmail.com>
2020-03-28 03:23:32 -0700

    beta version - figure out how to disable wasd and jijkl

--- Keyboard Cursor/Control.h
+++ Keyboard Cursor/Control.h
@@ -19,11 +19,43 @@ void move(int dx, int dy) {
 }
 
 void EventControl() {
-	using namespace std; ////
+	using namespace std;
+	INPUT leftclick;
+	leftclick.type = INPUT_MOUSE;
+	leftclick.mi.dwFlags = (MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP);
+	leftclick.mi.mouseData = 0;
+	leftclick.mi.dwExtraInfo = NULL;
+	leftclick.mi.time = 0;
+
+	INPUT rightclick = leftclick;
+	rightclick.mi.dwFlags = (MOUSEEVENTF_RIGHTDOWN | MOUSEEVENTF_RIGHTUP);
+
+	INPUT f10;
+	WORD vkey = VK_F10;
+	f10.type = INPUT_KEYBOARD;
+	f10.ki.wScan = MapVirtualKey(vkey, MAPVK_VK_TO_VSC);
+	f10.ki.time = 0;
+	f10.ki.dwExtraInfo = 0;
+	f10.ki.wVk = vkey;
+	f10.ki.dwFlags = 0; 
+
+	INPUT keyup = f10;
+	keyup.ki.dwFlags = (KEYEVENTF_KEYUP);
+
+	bool select = true;
+	
 
 	while (true) {
+		if ((GetKeyState(VK_LBUTTON) & 0x100) != 0) {
+			select = true;
+		}
 		int tdiff = (GetKeyState(VK_LSHIFT) & 0x8000) ? diff + boost : diff; // if shift is pressed, add boost to diff
 		if (GetKeyState(VK_CAPITAL) == 1) { // state of caps lock == 1 if it is toggled
+			if (select) { // deselect everything
+				SendInput(1, &f10, sizeof(INPUT));
+				select = false;
+			}
+			
 			if ((GetKeyState(0x41) & 0x8000 || GetKeyState(0x4A) & 0x8000) &&
 				(GetKeyState(0x57) & 0x8000 || GetKeyState(0x49) & 0x8000)) { // left-up
 				move(-tdiff, -tdiff);
@@ -61,7 +93,25 @@ void EventControl() {
 				move(0, tdiff);
 				this_thread::sleep_for(sleeptime);
 			}
+
+			if (GetKeyState(0x51) & 0x8000 || GetKeyState(0x55) & 0x8000) { // 'q' or 'u' pressed = left-mouse-button
+				SendInput(1, &leftclick, sizeof(INPUT));
+				this_thread::sleep_for(chrono::milliseconds(300));
+			}
+			if (GetKeyState(0x45) & 0x8000 || GetKeyState(0x4F) & 0x8000) { // 'e' or 'o' pressed = right-mouse-button
+				SendInput(1, &rightclick, sizeof(INPUT));
+				this_thread::sleep_for(chrono::milliseconds(300));
+			}
+			
 		}
+		else {
+			select = true;
+		}
+		if (GetKeyState(VK_F10) & 0x8000) {
+			SendInput(1, &keyup, sizeof(INPUT));
+		}
+
+
 	}
 }
 
--- Keyboard Cursor/Launch.cpp
+++ Keyboard Cursor/Launch.cpp
@@ -5,17 +5,10 @@
 #include<conio.h>
 #include<Windows.h>
 
-void fun(int times) {
-	for (int i = 0; i <= times; i++) {
-		move(5, 5);
-		std::this_thread::sleep_for(std::chrono::milliseconds(100));
-	}
-}
 
 int main() {
 	using namespace std;
 	thread t1(EventControl);
-	
 
 	cin.get();
 	t1.detach();
